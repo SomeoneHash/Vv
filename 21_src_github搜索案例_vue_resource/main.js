3a76144d2cdc67fb96657e0e431128914f8523a8
liujiwen <1010068051@qq.com>
2021-07-29 21:11:52 +0800

    'router'

--- 21_src_github搜索案例_vue_resource/main.js
+++ 21_src_github搜索案例_vue_resource/main.js
@@ -1,17 +1,17 @@
 //引入Vue
 import Vue from "vue"
 import App from "./App.vue"
-//引入vue-router
-import VueRouter from 'vue-router'
-//引入router
-import router from './router'
+import vueResource from 'vue-resource'
 
 Vue.config.productionTip = false
-Vue.use(VueRouter)
+
+Vue.use(vueResource)
 
 new Vue({
   el:'#app',
   render: h => h(App),
-  router
+  beforeCreate() {
+    Vue.prototype.$bus = this
+  },
 })
 
