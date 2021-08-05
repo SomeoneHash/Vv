de694d04b846a15b0f2ec3e8b2952204eb2cc017
liujiwen <1010068051@qq.com>
2021-08-05 22:29:34 +0800

    '~~~'

---
+++ src~~~/components/myIte.vue
@@ -0,0 +1,73 @@
+<template>
+  <li>
+    <label>
+      <input type="checkbox" :checked='todo.isDone' @change='handleCheck(todo.id)'/>
+      <span>{{todo.title}}</span>
+    </label>
+    <button class="btn-danger" @click="handleDelete(todo.id)">删除</button>
+  </li>
+</template>
+
+<script>
+  export default {
+    name:'myItem',
+    props:['todo','checkTodo','deleteTodo'],
+    methods:{
+      handleCheck(id){
+        this.checkTodo(id)
+      },
+      handleDelete(id){
+        if(confirm('确定要删除？')){
+          this.deleteTodo(id)
+        }
+      },
+    }
+  };
+</script>
+
+<style>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+ul li button {
+  float: right;
+  display: none;
+  margin-top: 7px;
+}
+
+li:before {
+  content: initial;
+}
+
+
+li:last-child {
+  border-bottom: none;
+}
+
+li:hover {
+  background-image: linear-gradient(to top, #5ee7df 0%, #b490ca 100%);
+}
+
+li:hover button {
+  display: block;
+}
+
+</style>
---
+++ src~~~/components/myList.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul class="todo-main">
+    <myItem 
+			v-for="todoObj in todos"
+			:key="todoObj.id" 
+			:todo="todoObj" 
+			:checkTodo="checkTodo"
+      :deleteTodo='deleteTodo'
+		/>
+  </ul>
+</template>
+
+<script>
+
+  import myItem from './myIte.vue';
+
+  components:{ myItem }
+  export default {
+    name:'myList',
+    components: { myItem },
+    props:['todos','checkTodo','deleteTodo']
+
+  };
+</script>
+
+<style>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
+
+
+
